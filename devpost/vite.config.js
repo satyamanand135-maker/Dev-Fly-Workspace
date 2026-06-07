83e0987af57a39126b03f38ff652a00966058891
Satyam Anand <satyamanand135@gmail.com>
2026-06-07 12:56:15 +0530

    removing some plugins for frontend publishing

--- devpost/vite.config.js
+++ devpost/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-oxc' // Update this import
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
